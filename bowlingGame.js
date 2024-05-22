71407d5f973a9d7b7520fcab090ce8c2ff7817c5
michael.li <michael.li@thoughtworks.com>
2024-05-22 11:00:20 +0800

    add second test case All Spares with first throw knocking down 5 pins each and make it pass

--- bowlingGame.js
+++ bowlingGame.js
@@ -1,5 +1,35 @@
 function calculateBowlingScore(frames) {
-    return 300;  // Hardcoded to pass the initial test
+    let score = 0;
+    let rolls = frames.split(" ").join("").split("");
+    let rollIndex = 0;
+
+    for (let frame = 0; frame < 10; frame++) {
+        if (rolls[rollIndex] === 'X') {  // Strike
+            score += 10 + strikeBonus(rolls, rollIndex);
+            rollIndex++;
+        } else if (rolls[rollIndex + 1] === '/') {  // Spare
+            score += 10 + spareBonus(rolls, rollIndex);
+            rollIndex += 2;
+        } else {  // Open frame
+            score += openFrameScore(rolls, rollIndex);
+            rollIndex += 2;
+        }
+    }
+
+    return score;
+}
+
+function strikeBonus(rolls, index) {
+    return (rolls[index + 1] === 'X' ? 10 : parseInt(rolls[index + 1])) +
+           (rolls[index + 2] === 'X' ? 10 : (rolls[index + 2] === '/' ? 10 - parseInt(rolls[index + 1]) : parseInt(rolls[index + 2])));
+}
+
+function spareBonus(rolls, index) {
+    return rolls[index + 2] === 'X' ? 10 : parseInt(rolls[index + 2]);
+}
+
+function openFrameScore(rolls, index) {
+    return parseInt(rolls[index]) + parseInt(rolls[index + 1]);
 }
 
 module.exports = calculateBowlingScore;
--- bowlingGame.test.js
+++ bowlingGame.test.js
@@ -1,8 +1,17 @@
 const calculateBowlingScore = require('./bowlingGame');
 
-test('All Strikes - Perfect Game', () => {
-    const frames = 'X X X X X X X X X XXX';
-    const expectedScore = 300;
-    const score = calculateBowlingScore(frames);
-    expect(score).toBe(expectedScore);
+describe('Bowling Game Score Calculator', () => {
+    test('All Strikes - Perfect Game', () => {
+        const frames = 'X X X X X X X X X XXX';
+        const expectedScore = 300;
+        const score = calculateBowlingScore(frames);
+        expect(score).toBe(expectedScore);
+    });
+
+    test('All Spares with first throw knocking down 5 pins each', () => {
+        const frames = '5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5';
+        const expectedScore = 150;
+        const score = calculateBowlingScore(frames);
+        expect(score).toBe(expectedScore);
+    });
 });
